59117e65c266e7ff56b07a7b6c7a5698bd70ed33
hailin chen <hchen256@my.bcit.ca>
2022-03-18 13:36:25 -0700

    Updated detail page get  rates from database

--- scripts/rest_detail.js
+++ scripts/rest_detail.js
@@ -12,8 +12,11 @@ db.collection("Restaurants").where("id", "==", restID)
     if ((size == 1)) {
       var thisRest = Rests[0].data();
       name = thisRest.name;
+      rate = thisRest.rating;
       console.log(name);
       document.getElementById("Rest_Name").innerHTML = name;
+      document.getElementById("rating").innerHTML = rate;
+
     } else {
       console.log("Query has more than one data");
     }
