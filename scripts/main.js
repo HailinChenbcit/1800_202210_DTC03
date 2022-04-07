fe302b4f72ea3ed608959bea7ebb6321b2ddd48e
hailin chen <hchen256@my.bcit.ca>
2022-04-06 21:46:54 -0700

    Updated main

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,8 @@
 var currentUser;
+
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
     currentUser = db.collection("users").doc(user.uid); //global
-    console.log(currentUser);
-
     // the following functions are always called when someone is logged in
     insertName();
   } else {
@@ -13,16 +12,14 @@ firebase.auth().onAuthStateChanged((user) => {
   }
 });
 
-
+// show user name in main page
 function insertName() {
     // to check if the user is logged in:
     currentUser.get().then((userDoc) => {
       //get the user name
       var user_Name = userDoc.data().name;
-      console.log(user_Name);
       $("#name-goes-here").text(user_Name); //jquery
-      // document.getElementByID("name-goes-here").innerText=user_Name;
     });
   }
 
-insertName
+insertName()
